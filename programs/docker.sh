e2393fe4b82817a029640d1fe2ca9592d965f62a
Jenczu11 <bartek.jencz@wp.pl>
2020-01-07 20:34:18 +0100

    Finished docker script

--- programs/docker.sh
+++ programs/docker.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-
+function print(){
+    printf "\033[1;32m$1\033[0m\n" 
+}
+print "Running sudo apt update"
 sudo apt update
+print "Running installing stuff for docker"
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
+print "Downloading gpg key"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   disco \
    stable"
+print "Running sudo apt update"
 sudo apt update
+print "Installing Docker"
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 
+print "Downloading dockerCompose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+print "chmod +x on docker compose"
 sudo chmod +x /usr/local/bin/docker-compose
-
+print "dockerCompose --version" 
 docker-compose --version
